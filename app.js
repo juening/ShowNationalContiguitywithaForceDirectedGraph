6ad28bc460af04131d97491cc8b8548d3f5bd558
Jue Ning <juening@gmail.com>
2016-07-16 02:34:07 -0400

    done

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var dataUrl = "https://raw.githubusercontent.com/DealPete/forceDirected/master/c
 
 
 var w = 900, h = 600;
-var svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
+var svg = d3.select("#graph").append("svg").attr("width", w).attr("height", h);
 
 d3.json(dataUrl, function(error, data){
     if(error){
@@ -13,24 +13,38 @@ d3.json(dataUrl, function(error, data){
                       .nodes(data.nodes)
                       .links(data.links)
                       .size([w, h])
-                      .linkDistance([50])
-                      .charge([-100]) //repel each other
-                      .start();
+                      .linkDistance(50)
+                      .charge(-100).start(); //repel each other
+                     
     
-        var edges = svg.selectAll("line")
+        var edges = svg.selectAll(".link")
                        .data(data.links)
                        .enter()
                        .append("line")
                        .style("stroke", "#ccc")
                        .style("stroke-width", 1);
         
-        var nodes = svg.selectAll("circle")
-                       .data(data.nodes)
-                       .enter()
-                       .append("circle")
-                       .attr("r", 10)
-                       .style("fill", function(d, i){return colors(i);})
-                       .call(force.drag);//enable drag and drop
+
+        
+        var nodes = d3.select("#flags").selectAll('.node').data(data.nodes).enter().append('img')
+                      .attr('class', function (d) {
+                          return 'flag flag-' + d.code;
+                          })
+                       .on("mouseover", function(d){                           
+                           var xPosition = d3.event.pageX;
+                           var yPosition = d3.event.pageY + 14;
+                           var countryName = $("#name");
+                           countryName.html("<div>" + d.country + "</div>");
+                           countryName.css({left:xPosition, top: yPosition, display:"block"});                        
+                       }).on("mouseout", function(){
+                           var countryName = $("#name");
+                           countryName.css("display", "none")
+                       })
+                      .call(force.drag);
+
+
+
+
         
         force.on("tick", function(){
             edges.attr("x1", function(d) { return d.source.x; })
@@ -38,8 +52,8 @@ d3.json(dataUrl, function(error, data){
                  .attr("x2", function(d) { return d.target.x; })
                  .attr("y2", function(d) { return d.target.y; });
 
-            nodes.attr("cx", function(d) { return d.x; })
-                 .attr("cy", function(d) { return d.y; });
+          nodes.style('top',function(d){return d.y + 'px'})
+               .style('left',function(d){return d.x -6 + 'px'})
         });
     }
 })
